a3f9aac41209590501bfedf8ba46a3ad6e0d6b83
Grant Ramsay <seapagan@gmail.com>
2017-06-27 20:41:17 +0100

    Add Git PPA without confirmation
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ THISPATH="$(dirname $(readlink -f "$0"))"
 echo "We are running from : $THISPATH"
 
 # ensure we have the latest packages, including git and sublime text repos
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:git-core/ppa -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
