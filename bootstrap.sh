c722fbe946ac14300bfb1e0a2cdcafa5705183e7
Grant Ramsay <seapagan@gmail.com>
2017-09-24 19:06:44 +0100

    Add nvm bash-completion to current shell
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -77,6 +77,7 @@ echo "# Set up NVM" >> ~/.bashrc
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm install node
 nvm use --lts
